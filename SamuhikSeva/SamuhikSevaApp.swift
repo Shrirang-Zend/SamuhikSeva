c3afd9b95bd1241962e7568119ed04a58b5c4bff
Shrirang-Zend <128277571+Shrirang-Zend@users.noreply.github.com>
2025-01-22 11:49:44 +0530

    Initial Commit

---
+++ SamuhikSeva/SamuhikSevaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamuhikSevaApp.swift
+//  SamuhikSeva
+//
+//  Created by Shrirang Zend on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SamuhikSevaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
